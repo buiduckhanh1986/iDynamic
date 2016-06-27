ab7a5675e61f83c204709c47048107a26fd91e45
Bui Duc Khanh <buiduckhanh@hos-macbook-pro.local>
2016-06-27 23:16:18 +0700

    Initial Commit

---
+++ iDynamic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iDynamic
+//
+//  Created by Bui Duc Khanh on 6/27/16.
+//  Copyright © 2016 Bui Duc Khanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
